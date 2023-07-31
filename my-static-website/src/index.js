5fcb712382d5878d75ae84f0331fe61a6f70ef5d
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-07-30 23:50:21 -0300

    Website base structure

---
+++ my-static-website/src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, this is my static page with React!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ my-static-website/webpack.config.js
@@ -0,0 +1,22 @@
+// webpack.config.js
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+      ],
+    },
+  };  
