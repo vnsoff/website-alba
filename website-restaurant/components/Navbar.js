1c0757b2466d4d2d61a48b4cb683ccd839351c2c
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-11-03 00:50:15 -0300

    Navbar responsive mobile

--- website-restaurant/components/Navbar.js
+++ website-restaurant/components/Navbar.js
@@ -1,90 +1,69 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'styles/tailwind.css';
+import { XIcon as XMarkIcon, MenuAlt2Icon as MenuAlt3Icon } from '@heroicons/react/outline';
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
-  const menuRef = useRef();
 
   const handleMobileMenu = () => {
     setIsMobile(!isMobile);
   };
 
-  const handleClickOutside = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
-      setIsMobile(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
   return (
     <div className="relative">
-      <div
-        className="h-96 bg-cover bg-center"
-        style={{
-          backgroundImage: 'url("/images/header/header-bg.png")',
-          height: '800px',
-        }}
-      >
-        <nav
-          className={`lg:mx-40 lg:px-40 top-5 inset-x-0 flex items-center justify-between p-6 ${
-            isMobile ? 'bg-gray-800' : 'bg-gradient-to-b from-Stone-950'
-          }`}
-        >
+      <div className="h-96 bg-cover bg-center" style={{ 
+        backgroundImage: 'url("/images/header/header-bg.png")',
+        height: '800px'
+      }}>
+        <nav className={`lg:mx-40 lg:px-40 top-5 inset-x-0 flex items-center justify-between p-6 ${isMobile ? 'bg-gray-800' : 'bg-gradient-to-b from-Stone-950'}`}>
           <div className="container mx-5 lg:mx-auto flex items-center justify-between">
-            <a href="#page-top">
-              <img src="/images/header/navbar-logo.png" alt="Logo" className="h-8" />
-            </a>
+            <div className="flex items-center">
+              {!isMobile && (
+                <a href="#page-top">
+                  <img src="/images/header/navbar-logo.png" alt="Logo" className="h-8" />
+                </a>
+              )}
+              {isMobile ? <br /> : null}
+            </div>
             <button
               className="text-white lg:hidden focus:outline-none"
               aria-label="Toggle navigation"
               onClick={handleMobileMenu}
             >
               {isMobile ? (
-                <span className="text-white">&#x2715;</span>
+                <XMarkIcon className="w-7 h-7 text-white" />
               ) : (
-                <span className="text-white">&#9776;</span>
+                <MenuAlt3Icon className="w-7 h-7 text-white"/>
               )}
             </button>
+            <div className={`text-stone-200 lg:flex lg:items-center lg:w-auto ${isMobile ? 'block' : 'hidden'}`}>
+              {/* Design for the toggle menu content when isMobile is true */}
+              <ul>
+                <a className="nav-link mr-5 lg:mr-10" href="#services">
+                  SERVICES
+                </a>
+                <a className="nav-link mr-5 lg:mr-10" href="#portfolio">
+                  LA NOSTRA CUCINA
+                </a>
+                <a className="nav-link mr-5 lg:mr-10" href="#portfolio">
+                  OUR LEGACY
+                </a>
+                <a className="nav-link mr-5 lg:mr-10" href="#portfolio">
+                  MEET THE MONTI
+                </a>
+                <a className="nav-link lg:mr-10" href="#portfolio">
+                  RESERVATIONS
+                </a>
+              </ul>
+            </div>
           </div>
         </nav>
-        <div
-          ref={menuRef}
-          className={`lg:hidden fixed inset-y-0 right-0 z-10 w-3/4 bg-gray-800 transform ${
-            isMobile ? 'translate-x-0' : 'translate-x-full'
-          } transition duration-300 ease-in-out`}
-        >
-          <div className="flex flex-col h-screen justify-center items-start space-y-8 p-8">
-            <a className="text-white text-2xl" href="#services">
-              SERVICES
-            </a>
-            <a className="text-white text-2xl" href="#portfolio">
-              LA NOSTRA CUCINA
-            </a>
-            <a className="text-white text-2xl" href="#portfolio">
-              OUR LEGACY
-            </a>
-            <a className="text-white text-2xl" href="#portfolio">
-              MEET THE MONTI
-            </a>
-            <a className="text-white text-2xl" href="#portfolio">
-              RESERVATIONS
-            </a>
-          </div>
-        </div>
         <div className="flex items-center justify-center h-full lg:pl-20 lg:pr-20">
           <div className="text-center">
             <h2 className="text-4xl italic text-white">Discover Italy at Ristorante Alba</h2>
             <br></br>
             <br></br>
-            <h2 className="text-7xl font-bold drop-shadow-md text-white uppercase">
-              BENVENUTI ALLA NOSTRA CASA
-            </h2>
+            <h2 className="text-7xl font-bold drop-shadow-md text-white uppercase">BENVENUTI ALLA NOSTRA CASA</h2>
           </div>
         </div>
       </div>
