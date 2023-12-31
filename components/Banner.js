60c9c6bc8759b3a5d643b9a07cc8d86ad0cd6fd7
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-12-31 03:32:06 -0300

    Fix banners responsive mobile layout

--- components/Banner.js
+++ components/Banner.js
@@ -1,34 +1,35 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Image from 'next/image';
 
 function Banner() {
+  const images = [
+    { src: '/images/marketing/banner1.png', link: 'https://www.linkedin.com/feed/' },
+    { src: '/images/marketing/banner1.png', link: 'https://www.linkedin.com/feed/' },
+    { src: '/images/marketing/banner1.png', link: 'https://www.linkedin.com/feed/' },
+    { src: '/images/marketing/banner1.png', link: 'https://www.linkedin.com/feed/' },
+    { src: '/images/marketing/banner1.png', link: 'https://www.linkedin.com/feed/' },
+  ];
+
   return (
-    <Carousel
-      showArrows={true}
-      showStatus={false}
-      showThumbs={false}
-      infiniteLoop={true}
-      autoPlay={true}
-      interval={5000}
-      transitionTime={500}
-      emulateTouch={true}
-    >
-      <div>
-        <img src="/images/marketing/banner1.png" alt="Banner 1" />
-      </div>
-      <div>
-        <img src="/images/marketing/banner1.png" alt="Banner 2" />
-      </div>
-      <div>
-        <img src="/images/marketing/banner1.png" alt="Banner 3" />
-      </div>
-      <div>
-        <img src="/images/marketing/banner1.png" alt="Banner 4" />
-      </div>
-      <div>
-        <img src="/images/marketing/banner1.png" alt="Banner 5" />
-      </div>
-    </Carousel>
+      <Carousel
+          showArrows={true}
+          showStatus={false}
+          showThumbs={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={5000}
+          transitionTime={500}
+          emulateTouch={true}
+      >
+        {images.map((image, index) => (
+            <a key={index} href={image.link} target="_blank" rel="noopener noreferrer">
+              <div>
+                <Image src={image.src} alt={`Banner ${index + 1}`} width={1920} height={1080} />
+              </div>
+            </a>
+        ))}
+      </Carousel>
   );
 }
 
--- components/TeamMember.js
+++ components/TeamMember.js
@@ -6,20 +6,20 @@ import { TwitterIcon, FacebookIcon, LinkedInIcon } from './IconComponent'; // Im
 
 const TeamMember = ({ name, role, imgSrc, twitterLink, facebookLink, linkedinLink }) => {
   return (
-    <main id="about">
-      <div className="col-lg-4 text-center mb-8">
-        <div className="team-member">
-          <img className="mx-auto rounded-full h-64 w-64 mb-4 border-8 border-slate-200" src={imgSrc} alt={name} />
-          <h4 className="text-lg font-bold">{name}</h4>
-          <p className="text-sm text-gray-600">{role}</p>
-          <div className="flex justify-center gap-x-2 mt-4">
-            <TwitterIcon/>
-            <FacebookIcon/>
-            <LinkedInIcon/>
+      <main id="about">
+        <div className="col-lg-4 text-center mb-8">
+          <div className="team-member">
+            <img className="mx-auto rounded-full h-64 w-64 mb-4 border-8 border-slate-200" src={imgSrc} alt={name} />
+            <h4 className="text-lg font-bold">{name}</h4>
+            <p className="text-sm text-gray-600">{role}</p>
+            <div className="flex justify-center gap-x-2 mt-4">
+              <a href={twitterLink} target="_blank" rel="noopener noreferrer"><TwitterIcon/></a>
+              <a href={facebookLink} target="_blank" rel="noopener noreferrer"><FacebookIcon/></a>
+              <a href={linkedinLink} target="_blank" rel="noopener noreferrer"><LinkedInIcon/></a>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
   );
 };
 
